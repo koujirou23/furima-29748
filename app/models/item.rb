8b6ae613f336101871373d9118d5a471690cdbb1
koujirou23 <kou23ilike5296@gmail.com>
2020-09-23 15:16:47 +0900

    ActiveHashの導入、カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :area, :status, :delivery_fee, :day
+
+  validates :name, :text, :category, :status, :delivery_fee, :area, :day, :price, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :area_id, :date_id, numericality: { other_than: 1 } 
   belongs_to :user
   has_one :purchase
 end
