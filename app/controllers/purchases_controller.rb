d55331f197882a8a9e0fa142e2c1f87346f4969e
koujirou23 <kou23ilike5296@gmail.com>
2020-09-29 14:58:18 +0900

    payjpの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,27 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      card: order_params[:token],
+      currency:'jpy'
+    )
+   end
 
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,6 @@
 class Purchase < ApplicationRecord
+  attr_accessor :token
+  validates :token, presence: true
   has_one :address
   belongs_to :item
   belongs_to :user
