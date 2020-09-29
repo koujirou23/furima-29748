a1a32b5055918296908c4aa07d924f540f5a8f02
koujirou23 <kou23ilike5296@gmail.com>
2020-09-29 11:53:05 +0900

    addressのバリデーション,view画面の編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,6 +4,8 @@ class Address < ApplicationRecord
 
   validates :postcode, :area, :city, :road, :phone, presence: true
   validates :area_id { other_than: 0, message: 'Select' }
+  validates :phone, format: { with: ^0\d message: "number is invalid. Include half-width numbers "} 
+  validates :postcode, format: { with: ^\d{3}-\d{4} "is invalid. Include hyphen(-)" }
 
   belongs_to :purchase
 end
